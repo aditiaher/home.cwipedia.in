2d97731b97aae72bd9eafaf8e8b858c7abe1ff63
Shivam <shivamhande0000@gmail.com>
2021-08-12 22:32:00 +0530

    head re-add

--- src/Components/head.component.js
+++ src/Components/head.component.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
-
+import Typical from 'react-typical'
+// import logo from "../assets/logo/cplogo.png" 
+import headimg from "../assets/head.gif"
 const Head =()=>{
     return(
 
-        <div>
-
-
+        <div className="container text-center">
+            <img className="img-fluid vert-move"  src={headimg}  />
+                <Typical className="display-6"
+                        steps={['cwipedia', 1000, 'Diploma Engineering Portal', 700,'Community', 500,'Software Labs', 500,'We are the TEAM of talented programmers', 500]}
+                        loop={Infinity}
+                        wrapper="p"
+                />
         </div>
     );
 }
--- src/Components/navbar.component.js
+++ src/Components/navbar.component.js
@@ -1,12 +1,15 @@
+import React, {useState} from "react";
+import DarkModeToggle from "react-dark-mode-toggle";    
 
 
 const Navbar = ()=>{
     
-
+    const [isDarkMode, setIsDarkMode] = useState(() => false);
     return(
         <div>
             <nav className="navbar navbar-expand-lg navbar-light shadow p-3 mb-5">
                 <div className="container-fluid cnav">
+                    {/* <img src={} className="img-fluid" /> */}
                     <a className="navbar-brand" href="#">Home</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -22,12 +25,18 @@ const Navbar = ()=>{
                     </ul>
                     <div className="d-flex justify-content-center">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <a className="nav-link btn" aria-current="page" href="#">SignIn</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link btn" aria-current="page"  href="#">SignUp</a>
-                        </li>
+                        </li> */}
+                            {/* <DarkModeToggle
+                                onChange={setIsDarkMode}
+                                checked={isDarkMode}
+                                size={60}
+                                /> */}
+
                     </ul>
                     </div>
                     </div>
