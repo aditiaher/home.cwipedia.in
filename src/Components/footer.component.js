dc72816982e2025b777fe48afad647c64d299f7e
Shivam <shivamhande0000@gmail.com>
2021-08-16 18:42:24 +0530

    able to pub

--- src/Components/footer.component.js
+++ src/Components/footer.component.js
@@ -4,7 +4,15 @@ const Footer =()=>{
         <div className="text-center" >
             <footer>
         <div className="container overflow-hidden">
+        <a aria-hidden='true' class='fa fa-envelope' href='mailto:business@cwipedia.in' id='devso' rel='noreferrer' target='_blank' title='mail us'></a>
+        <a aria-hidden='true' class='fa fa-instagram' href='http://instagram.com/cwipedia.in' id='devso' rel='noreferrer' target='_blank' title='follow us'></a>
+        <a aria-hidden='true' class='fa fa-twitter' href='https://twitter.com/cwipedia' id='devso' rel='noreferrer' target='_blank' title='follow us'></a>
+        <a aria-hidden='true' class='fa fa-linkedin' href='https://www.linkedin.com/company/cwipedia' id='devso' rel='noreferrer' target='_blank' title='follow us'></a>
+        <a aria-hidden='true' class='fa fa-telegram' href='https://telegram.me/msbteresources' id='devso' rel='noreferrer' target='_blank' title='Join group'></a>
+        <a aria-hidden='true' class='fa fa-youtube-play' href='https://www.youtube.com/channel/UCzARaaZ1furvIwUaNUsORWQ' id='devso' rel='noreferrer' target='_blank' title='follow us'></a>
+        <hr/>
         <div className="row gx-5">
+       
             <div className="col-sm">
 
                 <p className="h4">cwipedia.in</p>
@@ -14,20 +22,20 @@ const Footer =()=>{
             </div>
             <div className="col-sm">
 
-                <a className="link">Career</a><br/>
-                <a className="link">Training</a>
+                <a href="https://career.cwipedia.in" className="link">Career</a><br/>
+                <a href="https://training.cwipedia.in" className="link">Training</a>
 
             </div>
             <div className="col-sm">
 
-                <a className="link">Terms</a><br/>
-                <a className="link">Contact</a>
+                <a href="https://www.cwipedia.in/p/terms-and-conditions.html" className="link">Terms</a><br/>
+                <a href="https://www.cwipedia.in/p/contact-us-demofont-familyarial.html" className="link">Contact</a>
 
             </div>
 
             <hr/>
             <p>
-            Copyright &#169;2017-2021 Some rights reserved | <a className="link" href='https://www.cwipedia.in' >cwipedia.in</a>
+            Copyright &#169;2017-2021 All rights reserved | <a className="link" href='https://www.cwipedia.in' >cwipedia.in</a>
             </p>
 
         </div>
--- src/Components/navbar.component.js
+++ src/Components/navbar.component.js
@@ -10,17 +10,20 @@ const Navbar = ()=>{
             <nav className="navbar navbar-expand-lg navbar-light shadow p-3 mb-5">
                 <div className="container-fluid cnav">
                     {/* <img src={} className="img-fluid" /> */}
-                    <a className="navbar-brand" href="#">Home</a>
+                    <a className="navbar-brand" href="/">Home</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Services</a>
+                        <a className="nav-link active" aria-current="page" href="https://www.cwipedia.in">blog</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="https://career.cwipedia.in">career</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="https://www.cwipedia.in/p/contact-us-demofont-familyarial.html">Contact</a>
                         </li>
                     </ul>
                     <div className="d-flex justify-content-center">
